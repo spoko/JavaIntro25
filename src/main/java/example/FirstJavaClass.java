901f631f2b94d57df4fa48e36bc94d56ec36224e
Ibraim <Ibraim2.0@gmail.com>
2025-01-15 19:49:48 +0200

    Initial commit

---
+++ src/main/java/example/FirstJavaClass.java
@@ -0,0 +1,10 @@
+package example;
+
+public class FirstJavaClass {
+
+    public static void main (String[] args)
+    {
+        //this will print the text in the console
+        System.out.println("Hello world!");
+    }
+}
